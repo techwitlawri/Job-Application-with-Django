8a3f31aa7898bdde7e2addd257ab6554a918dc52
Mmachi Ezeh <mmachilawri@gmail.com>
2024-09-19 22:02:16 +0100

    Creating a view and template

---
+++ job_application/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Form(models.Model):
+    first_name = models.CharField(max_length=80)
+    last_name = models.CharField(max_length=80)
+    email = models.EmailField(max_length=80)
+    date = models.DateField()
+    occupation = models.CharField(max_length=80)
+
+
+    def __init__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+
